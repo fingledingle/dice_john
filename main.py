7adbe66e8050ad205df2052ba2eead2fad7f4545
mmmmmm <elenai@riseup.net>
2024-02-10 21:43:17 +0800

    semi finished

--- main.py
+++ main.py
@@ -16,35 +16,53 @@ def choose_value():
   return value
 
 
+# A = choose_value()
+# print(A)
+# B = choose_value()
+# print(B)
+
+#Firrst attempt
+
+
 def compare_followers():
+  from replit import clear
   from art import logo
-  from art import vs 
+  from art import vs
   correct_answer = True
   points = 0
   A = choose_value()
   while correct_answer:
+    
 
-    print (f"{logo}")
+    print(f"{logo}")
+    
+    if correct_answer and points > 0:
+      print(f"You're right! Current score: {points}")
+      
     B = choose_value()
-    print(f"Compare A: {A['name']}, a {A['description']}, from {A['country']} {A['follower_count']} ")
-    print (f"{vs}")
-    print(f"Against B: {B['name']}, a {B['description']}, from {B['country']} {B['follower_count']} ")
+    print(
+      f"Compare A: {A['name']}, a {A['description']}, from {A['country']} {A['follower_count']} "
+    )
+    print(f"{vs}")
+    print(
+      f"Against B: {B['name']}, a {B['description']}, from {B['country']} {B['follower_count']} "
+    )
 
     which_option = input(
-      "Who has more followers? Type 'A' or 'B': "
-    ).capitalize()
+      "Who has more followers? Type 'A' or 'B': ").capitalize()
 
     answer = changing_values(which_option, A, B)
+    clear()
+    
 
     if answer == A['follower_count']:
 
       if A['follower_count'] > B['follower_count']:
         points += 1
-        A = A
 
       elif A['follower_count'] < B['follower_count']:
+        print(f"{logo}")
         print("You got it wrong buddy, nice try tho!")
-        correct_answer = False
         break
 
     elif answer == B['follower_count']:
@@ -54,24 +72,11 @@ def compare_followers():
         A = B
 
       elif B['follower_count'] < A['follower_count']:
+        print(f"{logo}")
         print("You got it wrong buddy, nice try tho!")
-        correct_answer = False
         break
-
-    if correct_answer and points > 0:
-      print(f"You're right! Current score: {points}")
+  
+      
 
 
 compare_followers()
-
-
-
-
-
-
-
-
-
-
-
-
