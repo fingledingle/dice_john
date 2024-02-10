79746c7d9b555bc4cfd299fe0efb0a3b6aac14ff
mmmmmm <elenai@riseup.net>
2024-02-10 21:27:02 +0800

    er

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 from game_data import data
 
 
-def changing_values(which_option):
+def changing_values(which_option, A, B):
   if which_option == "A":
     answer = A['follower_count']
   elif which_option == "B":
@@ -20,29 +20,160 @@ def choose_value():
 # B = choose_value()
 # print(B)
 
-points = 0
-correct_answer = True
-while correct_answer == True:
-  A = choose_value()
+
+##Firrst attempt
+
+# def compare_followers():
+#   correct_answer = True
+#   points = 0
+#   while correct_answer:
+
+#     A = choose_value()
+#     B = choose_value()
+#     print(f"{points}")
+#     print(f"Compare A: {A['name']}, a {A['description']}, from {A['country']} {A['follower_count']} ")
+#     print(f"Compare B: {B['name']}, a {B['description']}, from {B['country']} {B['follower_count']} ")
+  
+#     which_option = input(
+#       "Which one of them has the highest number of followers? A or B \n"
+#     ).capitalize()
+  
+#     answer = changing_values(which_option, A, B)
+    
+#     if answer == A['follower_count']:
+      
+#       if A['follower_count'] > B['follower_count']:
+#         points += 1
+#         A = A
+        
+#       elif A['follower_count'] < B['follower_count']:
+#         print("You got it wrong buddy, nice try tho!")
+#         correct_answer = False
+#         break
+    
+#     elif answer == B['follower_count']:
+      
+#       if B['follower_count'] > A['follower_count']:
+#         points += 1
+#         A = B
+        
+#       elif B['follower_count'] < A['follower_count']:
+#         print("You got it wrong buddy, nice try tho!")
+#         correct_answer = False
+#         break
+
+#     if correct_answer and points > 0:
+#       print(f"You're right! Current score: {points}")
+
+      
+# compare_followers()
+
+
+
+
+
+
+  
+# game(changing_values, choose_value
+
+
+
+
+def john_continuation (A, points):
   B = choose_value()
-  print(f"Compare A: {A['name']}, a {A['description']}, from {A['country']}")
-  print(f"Compare B: {B['name']}, a {B['description']}, from {B['country']}")
-
+  print(f"{points}")
+  print(f"Compare A: {A['name']}, a {A['description']}, from {A['country']} {A['follower_count']} ")
+  print(f"Compare B: {B['name']}, a {B['description']}, from {B['country']} {B['follower_count']} ")
+  
   which_option = input(
-    "Which one of them has the highest number of followers? A or B"
+    "Which one of them has the highest number of followers? A or B \n"
   ).capitalize()
-
-  answer = changing_values(which_option)
+  
+  answer = changing_values(which_option, A, B)
   
   if answer == A['follower_count']:
+  
     if A['follower_count'] > B['follower_count']:
       points += 1
+  
     elif A['follower_count'] < B['follower_count']:
-      print("You lost")
-      correct_answer == False
+      print("You got it wrong buddy, nice try tho!")
+      john = False
+      correct_answer = False
+    
+  
   elif answer == B['follower_count']:
+  
     if B['follower_count'] > A['follower_count']:
       points += 1
+      A = B
+  
     elif B['follower_count'] < A['follower_count']:
-      print("You lost")
-      correct_answer == False
+      print("You got it wrong buddy, nice try tho!")
+      john = False
+      correct_answer = False
+    
+
+
+john = False
+points = 0
+A = choose_value()
+B = choose_value()
+print(f"{points}")
+print(f"Compare A: {A['name']}, a {A['description']}, from {A['country']} {A['follower_count']} ")
+print(f"Compare B: {B['name']}, a {B['description']}, from {B['country']} {B['follower_count']} ")
+
+which_option = input(
+  "Which one of them has the highest number of followers? A or B \n"
+).capitalize()
+
+answer = changing_values(which_option, A, B)
+
+if answer == A['follower_count']:
+
+  if A['follower_count'] > B['follower_count']:
+    john = True
+    points += 1
+    A = A
+
+  elif A['follower_count'] < B['follower_count']:
+    print("You got it wrong buddy, nice try tho!")
+    john = False
+    correct_answer = False
+    
+
+elif answer == B['follower_count']:
+
+  if B['follower_count'] > A['follower_count']:
+    john = True
+    points += 1
+    A = B
+
+  elif B['follower_count'] < A['follower_count']:
+    print("You got it wrong buddy, nice try tho!")
+    john = False
+    correct_answer = False
+    
+
+while john == True:
+  print(f"You're right! Current score: {points}")
+  john_continuation(A, points)
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
